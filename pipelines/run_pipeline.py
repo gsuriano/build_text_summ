3429c764909ed4b55e5dec68a6207ae995a075c6
Giuseppe Suriano <108187762+gsuriano@users.noreply.github.com>
2023-09-21 09:29:07 +0100

    Update run_pipeline.py

--- pipelines/run_pipeline.py
+++ pipelines/run_pipeline.py
@@ -18,7 +18,7 @@ import json
 import sys
 import traceback
 
-from pipelines._utils import get_pipeline_driver, convert_struct, get_pipeline_custom_tags
+from _utils import get_pipeline_driver, convert_struct, get_pipeline_custom_tags
 
 
 def main():  # pragma: no cover
