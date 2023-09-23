0423ee35e003c5912b377fbf3ec9647a28a6ba2c
Giuseppe Suriano <108187762+gsuriano@users.noreply.github.com>
2023-09-23 09:39:00 +0100

    Update pipeline.py

--- pipelines/abalone/pipeline.py
+++ pipelines/abalone/pipeline.py
@@ -163,7 +163,7 @@ def get_pipeline(
         name="ModelApprovalStatus", default_value="PendingManualApproval"
     )
 
-    PREPROCESSING_SCRIPT_LOCATION = "./preprocess.py"
+    PREPROCESSING_SCRIPT_LOCATION = "preprocess.py"
 
     processing_code = sagemaker_session.upload_data(
         PREPROCESSING_SCRIPT_LOCATION,
